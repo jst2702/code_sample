3a7e2069a66b6e547c63dc5878959210024066bf
jst2702 <35535685+jst2702@users.noreply.github.com>
2022-08-16 22:30:35 -0400

    Update example.py

--- trading/example.py
+++ trading/example.py
@@ -5,15 +5,21 @@ Created on Tue Jun 21 09:09:55 2022
 @author: 15409
 """
 from alpaca_trading import alpacaLimitTrader
+from dotenv import load_dotenv
 import os
-from TinyTitans.src.trading.alpaca_credentials import apca_api_key_id, apca_api_secret_key
 
-os.environ["APCA_API_KEY_ID"] = apca_api_key_id
-os.environ["APCA_API_SECRET_KEY"] = apca_api_secret_key
+load_dotenv(dotenv_path='trade.env')
 
 limit_trader = alpacaLimitTrader()
 
-portfolio_dict = {'NGL': 0.1, 'VTGN': 0.1, 'GEVO': 0.1,
-                  'BOLT': 0.2, 'OST': 0.2, 'ARAY': 0.2, 'LFMD': 0.1}
+portfolio_dict = {
+  'NGL': 0.1, 
+  'VTGN': 0.1, 
+  'GEVO': 0.1,
+  'BOLT': 0.2, 
+  'OST': 0.2, 
+  'ARAY': 0.2, 
+  'LFMD': 0.1
+}
 
 limit_trader.have_portfolio(portfolio_dict)
